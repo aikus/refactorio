aa35e213a2cb5722543942f23e0b6e3b03c2dca0
Rusakov Nikita <aikus@yandex.ru>
2019-01-06 00:48:00 +0500

    fix main file

--- src/main.php
+++ src/main.php
@@ -2,6 +2,6 @@
 
 declare (strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
-use RefactoringRobot\Refactorer;
+use Refactorio\Refactorer;
 $refactorer = new Refactorer();
 file_put_contents($argv[1], $refactorer->refact(file_get_contents($argv[1])));
